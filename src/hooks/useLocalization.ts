3659a56d38e7ee18243bd5e6551ca8a2647a9354
Alan <prossliu@gmail.com>
2024-06-25 01:34:58 +0930

    fix TranslationProps

--- src/hooks/useLocalization.ts
+++ src/hooks/useLocalization.ts
@@ -7,54 +7,8 @@ type Language = 'en' | 'zh';
 
 interface TranslationProps {
   home: string;
-  shareStories: string;
-  travelGuides: string;
-  joinCommunity: string;
-  where: string;
-  dates: string;
-  guests: string;
-  pets: string;
-  newPost: string;
-  houses: string;
-  flats: string;
-  hostels: string;
-  villas: string;
-  personalInfo: string;
-  properties: string;
-  favorites: string;
-  privacyAndSecurity: string;
-  settings: string;
-  signOut: string;
-  login: string;
-  signUp: string;
-  profile: string;
-  myBookings: string;
-  viewHistory: string;
-  message: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-  username: string;
-  joinDate: string;
-  submit: string;
-  forgotPassword: string;
-  alreadyHaveAccount: string;
-  createAccount: string;
-  signIn: string;
-  uploadPhoto: string;
-  isYourPetDewormed: string;
-  basicInfo: string;
-  name: string;
-  breed: string;
-  gender: string;
-  male: string;
-  female: string;
-  birthday: string;
-  description: string;
-  cancel: string;
-  save: string;
-  yes: string;
-  no: string;
+  news: string;
+  papers: string;
 }
 
 const translations: Record<Language, TranslationProps> = {
